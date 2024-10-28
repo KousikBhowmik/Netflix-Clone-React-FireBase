1b3d3869b96e83c9ae9ba4c2cbcf80597edf8964
Kousik Bhowmik <kousikbhowmik888@gmail.com>
2024-10-28 21:40:24 +0530

    inisilize the project

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Home from './pages/Home/Home.jsx'
+import Login from './pages/Login/Login.jsx'
+import Player from './pages/Plalyer/Player.jsx'
+
+
+
+const App = () => {
+  return (
+    <div>
+      <Home />
+    </div>
+  )
+}
+
+export default App
