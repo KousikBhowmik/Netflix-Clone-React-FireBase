fff4cc109316581bf410e40cd840cf2e9079961c
Kousik Bhowmik <kousikbhowmik888@gmail.com>
2024-10-29 15:34:06 +0530

    login fork ui part done

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,18 @@
-import React from 'react'
-import Home from './pages/Home/Home.jsx'
-import Login from './pages/Login/Login.jsx'
-import Player from './pages/Plalyer/Player.jsx'
-
-
+import React from "react";
+import Home from "./pages/Home/Home.jsx";
+import Login from "./pages/Login/Login.jsx";
+import Player from "./pages/Plalyer/Player.jsx";
+import { Routes, Route } from "react-router-dom";
 
 const App = () => {
   return (
     <div>
-      <Home />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,10 +1,58 @@
-import React from 'react'
-import './Login.css'
+import React, { useState } from "react";
+import "./Login.css";
+import assets from "../../assets/assets.js";
 
 const Login = () => {
-  return (
-    <div className='login'>Login</div>
-  )
-}
+  const [signState, setSignState] = useState("Sign In");
 
-export default Login
+  return (
+    <div className="login">
+      <img src={assets.logo} className="login-logo" />
+      <div className="login-form">
+        <h1>{signState}</h1>
+        <form>
+          {signState === "Sign Up" ? (
+            <input type="text" placeholder="Your name" />
+          ) : null}
+          <input type="email" placeholder="Your email" />
+          <input type="password" placeholder="Password" />
+          <button>{signState}</button>
+          <div className="form-help">
+            <div className="remember">
+              <input type="checkbox" />
+              <label htmlFor="">Remember Me</label>
+            </div>
+            <p>Need help?</p>
+          </div>
+        </form>
+        <div className="form-switch">
+          {signState === "Sign In" ? (
+            <p>
+              New to Netflix?{" "}
+              <span
+                onClick={() => {
+                  setSignState("Sign Up");
+                }}
+              >
+                Sign Up Now
+              </span>
+            </p>
+          ) : (
+            <p>
+              Already have account?{" "}
+              <span
+                onClick={() => {
+                  setSignState("Sign In");
+                }}
+              >
+                Sign In Now
+              </span>
+            </p>
+          )}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
