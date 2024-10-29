64dde53d901b80a360e027485c421196200b6551
Kousik Bhowmik <kousikbhowmik888@gmail.com>
2024-10-29 14:23:39 +0530

    home page ui part done

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,10 +1,37 @@
 import React from 'react'
 import './Navbar.css'
+import assets from '../../assets/assets.js'
+
+
 
 const Navbar = () => {
   return (
-    <div className='navbar'>Navbar</div>
-  )
+    <div className="navbar">
+      <div className="navbar-left">
+        <img src={assets.logo} />
+        <ul>
+          <li>Home</li>
+          <li>TV Shows</li>
+          <li>Movies</li>
+          <li>New & Popular</li>
+          <li>My List</li>
+          <li>Browse By Languages</li>
+        </ul>
+      </div>
+      <div className="navbar-right">
+        <img src={assets.search_icon} className="icons" />
+        <p>Children</p>
+        <img src={assets.bell_icon} className="icons" />
+        <div className="navbar-profile">
+          <img src={assets.profile_img} className="profile" />
+          <img src={assets.caret_icon}  />
+          <div className="dropdown">
+            <p>Sing out of Netflix</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Navbar
--- src/components/TitleCards/TitleCards.jsx
+++ src/components/TitleCards/TitleCards.jsx
@@ -1,9 +1,37 @@
-import React from 'react'
+import React, { useEffect, useRef } from 'react'
 import './TitleCards.css'
+import cards_data from '../../assets/cards/Cards_data.js';
 
-const TitleCards = () => {
+
+const TitleCards = ({title, catagory}) => {
+
+  const handleWheel = (e) => {
+    e.preventDefault();
+    cardsRef.current.scrollLeft += e.deltaY;
+  }
+
+  const cardsRef = useRef();
+
+  useEffect(()=> {
+    cardsRef.current.addEventListener("wheel", handleWheel);
+  },[]);
+
   return (
-    <div className='title-cards'>TitleCards</div>
+    <div className='title-cards'>
+      <h2>{title?title:"Popular on Netflix"}</h2>
+      <div className="card-list" ref={cardsRef}>
+        {
+          cards_data.map((card, index)=> {
+            return(
+              <div className='card' key={index}>
+                <img src={card.image} />
+                <p>{card.name}</p>
+              </div>
+            )
+          })
+        }
+      </div>
+    </div>
   )
 }
 
